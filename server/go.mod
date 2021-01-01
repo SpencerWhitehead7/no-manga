b6ab7cd16671d30edb34fd9f090a295423a1d3ca
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-12-31 22:56:06 -0500

    res: gin hello world

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/SpencerWhitehead7/no-manga/server
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
--- server/main.go
+++ server/main.go
@@ -1,8 +1,18 @@
 // Package main is a server for handling API requests for no-manga.com.
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	r.Run() // listen and serve on 8080
 }
