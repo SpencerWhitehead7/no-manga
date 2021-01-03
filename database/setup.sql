6ae9140981482fe83fa3ec5b6336bf1a06d68d17
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-01-03 01:24:42 -0500

    feat: sql column constraints

--- database/setup.sql
+++ database/setup.sql
@@ -1,13 +1,23 @@
+-- CUSTOM CHECKS
+
+CREATE OR REPLACE FUNCTION not_empty(v varchar) RETURNS boolean AS $$
+  SELECT TRIM(v) <> ''
+$$ LANGUAGE SQL STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION array_no_empties(v varchar[]) RETURNS boolean AS $$
+  SELECT v <> '{}' AND (NOT EXISTS (SELECT 1 FROM unnest(v) s WHERE s = NULL OR TRIM(s) = ''))
+$$ LANGUAGE SQL STRICT IMMUTABLE;
+
 -- REFERENCE TABLES
 
 CREATE TABLE "demo" (
   "name" varchar
-    PRIMARY KEY
+    PRIMARY KEY CONSTRAINT no_empty_name CHECK (not_empty("name"))
 );
 
 CREATE TABLE "job" (
   "name" varchar
-    PRIMARY KEY
+    PRIMARY KEY CONSTRAINT no_empty_name CHECK (not_empty("name"))
 );
 
 -- DATA TABLES
@@ -15,43 +25,60 @@ CREATE TABLE "job" (
 CREATE TABLE "magazine" (
   "id" int
     GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "name" varchar,
-  "other_names" varchar[],
-  "description" varchar,
+  "name" varchar
+    NOT NULL CONSTRAINT no_empty_name CHECK (not_empty("name")),
+  "other_names" varchar[]
+    CONSTRAINT no_empties_other_names CHECK (array_no_empties("other_names")),
+  "description" varchar
+    NOT NULL CONSTRAINT no_empty_description CHECK (not_empty("description")),
   "demo" varchar REFERENCES "demo"("name")
+    NOT NULL
 );
 
 CREATE TABLE "mangaka" (
   "id" int
     GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "name" varchar,
-  "other_names" varchar[],
+  "name" varchar
+    NOT NULL CONSTRAINT no_empty_name CHECK (not_empty("name")),
+  "other_names" varchar[]
+    CONSTRAINT no_empties_other_names CHECK (array_no_empties("other_names")),
   "description" varchar
+    NOT NULL CONSTRAINT no_empty_description CHECK (not_empty("description"))
 );
 
 CREATE TABLE "manga" (
   "id" int
     GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "name" varchar,
-  "other_names" varchar[],
-  "description" varchar,
-  "demo" varchar REFERENCES "demo"("name"),
-  "start_date" date,
+  "name" varchar
+    NOT NULL CONSTRAINT no_empty_name CHECK (not_empty("name")),
+  "other_names" varchar[]
+    CONSTRAINT no_empties_other_names CHECK (array_no_empties("other_names")),
+  "description" varchar
+    NOT NULL CONSTRAINT no_empty_description CHECK (not_empty("description")),
+  "demo" varchar REFERENCES "demo"("name")
+    NOT NULL,
+  "start_date" date
+    NOT NULL,
   "end_date" date
 );
 
 CREATE TABLE "chapter" (
-  "manga_id" int REFERENCES "manga"("id"),
-  "chapter_num" float(4),
-  "name" varchar,
-  "page_count" int,
-  "updated_at" timestamptz,
+  "manga_id" int REFERENCES "manga"("id")
+    NOT NULL,
+  "chapter_num" float(4)
+    NOT NULL,
+  "name" varchar
+    CONSTRAINT no_empty_name CHECK (not_empty("name")),
+  "page_count" int
+    NOT NULL,
+  "updated_at" timestamptz
+    NOT NULL,
   PRIMARY KEY ("manga_id", "chapter_num")
 );
 
 CREATE TABLE "genre" (
   "name" varchar
-    PRIMARY KEY
+    PRIMARY KEY CONSTRAINT no_empty_name CHECK (not_empty("name"))
 );
 
 -- RELATIONS
@@ -69,12 +96,14 @@ CREATE TABLE "manga_mangaka_job" (
   "mangaka_id" int REFERENCES "mangaka"("id") ON DELETE CASCADE
     NOT NULL,
   "job"  varchar REFERENCES "job"("name") ON DELETE CASCADE
-    NOT NULL
+    NOT NULL,
+  UNIQUE("manga_id", "mangaka_id")
 );
 
 CREATE TABLE "manga_genre" (
   "manga_id" int REFERENCES "manga"("id") ON DELETE CASCADE
     NOT NULL,
   "genre" varchar REFERENCES "genre"("name") ON DELETE CASCADE
-    NOT NULL
+    NOT NULL,
+  UNIQUE("manga_id", "genre")
 );
