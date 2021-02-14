70fd1b818f53d8d32c8b714a516ae101ec861cb9
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-02-13 23:30:01 -0500

    edit: neaten gqlgen config settings, add doc comments

--- server/graph/resolver.go
+++ server/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+// To run go generate recursively over entire project, use this command: go generate ./...
+//go:generate go run github.com/99designs/gqlgen
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
+// Resolver serves as dependency injection for no-manga's gqlResolvers.
 type Resolver struct{}
