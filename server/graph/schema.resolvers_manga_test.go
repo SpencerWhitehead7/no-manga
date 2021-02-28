f94a18b4b9f6a631a36f14a29df7fc960fa4815c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-02-28 14:58:45 -0500

    test: MangaList resolver

--- server/graph/schema.resolvers_manga_test.go
+++ server/graph/schema.resolvers_manga_test.go
@@ -49,3 +49,35 @@ func TestManga(t *testing.T) {
 
 	testhelpers.ClearDB(t, db)
 }
+
+func TestMangaListNoMangas(t *testing.T) {
+	w := testhelpers.CallGQL(r, `{"query":"{mangaList {id}}"}`)
+
+	expect := `{"data":{"mangaList":[]}}`
+	actual := w.Body.String()
+	if expect != actual {
+		t.Errorf("\nQueryResult\n  expect: %v\n  actual: %v", expect, actual)
+	}
+}
+
+func TestMangaList(t *testing.T) {
+	testhelpers.MangaFactory(t, db, testhelpers.MangaStub{
+		Name: "b", EndDate: "2020-01-01",
+	})
+	testhelpers.MangaFactory(t, db, testhelpers.MangaStub{
+		Name: "a", OtherNames: []string{"alpha"},
+	})
+
+	w := testhelpers.CallGQL(r, `{"query":"{mangaList {id name otherNames description demo startDate endDate}}"}`)
+
+	expect := `{"data":{"mangaList":[` +
+		`{"id":2,"name":"a","otherNames":["alpha"],"description":"tDescription","demo":"shonen","startDate":"2000-01-01T00:00:00Z","endDate":null},` +
+		`{"id":1,"name":"b","otherNames":[],"description":"tDescription","demo":"shonen","startDate":"2000-01-01T00:00:00Z","endDate":"2020-01-01T00:00:00Z"}` +
+		`]}}`
+	actual := w.Body.String()
+	if expect != actual {
+		t.Errorf("\nQueryResult\n  expect: %v\n  actual: %v", expect, actual)
+	}
+
+	testhelpers.ClearDB(t, db)
+}
