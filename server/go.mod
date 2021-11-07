c6a938bbc3306e3d0d79b5b66c46394674d8b81b
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-11-07 13:51:56 -0500

    res: database connection

--- server/go.mod
+++ server/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/graph-gophers/graphql-go v1.2.0
+	github.com/jackc/pgx/v4 v4.13.0
 )
--- server/main.go
+++ server/main.go
@@ -2,11 +2,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/jackc/pgx/v4/pgxpool"
 
 	"github.com/SpencerWhitehead7/no-manga/server/playground"
 	"github.com/SpencerWhitehead7/no-manga/server/resolver"
@@ -14,10 +18,30 @@ import (
 )
 
 func main() {
+	var err error
+
+	db, err := pgxpool.Connect(context.Background(), "postgresql://spencer:@localhost:5432/no-manga")
+	if err != nil {
+		log.Fatalln("unable to connect to database:", err)
+	}
+	err = db.Ping(context.Background())
+	if err != nil {
+		log.Fatalln("database startup ping failed:", err)
+	}
+	defer db.Close()
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+	r.GET("/dbping", func(c *gin.Context) {
+		err := db.Ping(c)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": fmt.Sprintf("database ping failed: %v", err)})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "dbpong"})
+		}
 	})
 
 	r.POST("/gql", gin.WrapH(&relay.Handler{
