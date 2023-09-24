b218b09225b263b6b59d57915881dcf353aaf540
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2023-09-24 13:56:59 -0400

    res: hybrid deployment on cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
 import { buildRedirects } from "./src/buildRedirects";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: cloudflare({
+    runtime: "local",
+  }),
   redirects: buildRedirects(),
 });
