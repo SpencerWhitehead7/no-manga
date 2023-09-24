b55a38caf015be078a66295a6c5ce4838d7c5cd3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2023-09-24 13:14:15 -0400

    feat: very, very basic routes/pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import { buildRedirects } from "./src/buildRedirects";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: buildRedirects(),
+});
---
+++ src/buildRedirects.ts
@@ -0,0 +1,6 @@
+// generated from DB query to redirect all manga/mangaka/magazine ID routes to id/name routes, someday
+export const buildRedirects = () => ({
+  "/mangaka/1": "/mangaka/1/kentaro-miura",
+  "/mangaka/2": "/mangaka/2/eichiro-oda",
+  "/mangaka/3": "/mangaka/3/shimoku-kio",
+});
