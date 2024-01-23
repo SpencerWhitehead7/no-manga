980df0a35bd1af38c99e94c27cb91f4153741da5
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2024-01-23 18:00:49 -0500

    feat: MPA reader view

--- src/consts.ts
+++ src/consts.ts
@@ -5,6 +5,15 @@ export const ROUTE = {
   SEARCH: () => "/search",
   MANGA_LIST: () => "/manga",
   MANGA: (manga: Manga) => `/manga/${manga.id}/${manga.slug}`,
+  MANGA_READ: (manga: Manga, chapterNum = 1, pageNum = 1) =>
+    `/manga/${manga.id}/${manga.slug}/read?chapter=${chapterNum}&page=${pageNum}`,
+  // to be replaced with cloudflare CDN
+  MANGA_READ_SRC: (manga: Manga, chapterNum: number, pageNum: number) => {
+    const pChapterNum = String(chapterNum).padStart(4, "0");
+    const pPageNum = String(pageNum).padStart(3, "0");
+
+    return `/imgs/${manga.id}/${pChapterNum}/${pPageNum}.jpg`;
+  },
   MANGAKA_LIST: () => "/mangaka",
   MANGAKA: (mangaka: Mangaka) => `/mangaka/${mangaka.id}/${mangaka.slug}`,
   MAGAZINE_LIST: () => "/magazine",
