44938c1fac300f024c0bff3d1043569c30cb2429
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2024-01-29 01:29:27 -0500

    feat: redirects for reader pages

--- src/buildRedirects.ts
+++ src/buildRedirects.ts
@@ -10,11 +10,24 @@ const createAssetRedirects = (
     return redirects;
   }, {});
 
+const createReaderRedirects = (
+  baseRoute: string,
+  assets: { id: number; slug: string }[],
+) =>
+  assets.reduce<Record<string, string>>((redirects, { id, slug }) => {
+    redirects[
+      `/${baseRoute}/${id}/read`
+    ] = `/${baseRoute}/${id}/${slug}/read?chapter=1&page=1`;
+
+    return redirects;
+  }, {});
+
 export const buildRedirects = () => {
   const repo = new BuildTimeRepo(import.meta.env.VITE_DB_PATH);
 
   return {
     ...createAssetRedirects("manga", repo.getAllMangas()),
+    ...createReaderRedirects("manga", repo.getAllMangas()),
     ...createAssetRedirects("mangaka", repo.getAllMangakas()),
     ...createAssetRedirects("magazine", repo.getAllMagazines()),
   };
